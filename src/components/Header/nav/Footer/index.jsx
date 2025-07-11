a3e237e82d51b48605227574886d4ac2040f21a2
unknown <dangabarin2020@gmail.com>
2025-07-11 18:23:10 +0100

    first commit

--- src/components/Header/nav/Footer/index.jsx
+++ src/components/Header/nav/Footer/index.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function index() {
   return (
-    <div className={` flex justify-between font-normal text-para  bg-slate-500 w-full text-brand-text `}>
+    <div className={` flex justify-between font-normal text-[12px] w-full text-brand-text `}>
         <Link href={"/"}>Awwwards</Link>
         <Link href={"/"}>Instagram</Link>
         <Link href={"/"}>Dribble</Link>
